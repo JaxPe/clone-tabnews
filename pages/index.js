76953c2e70fec633593b27296ee3bbc73740848a
JaxPe <jpbarroco21@gmail.com>
2025-11-11 11:41:23 -0300

    altera texto em `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 function Home() {
   return (
-    <>
-      <h1>Antonia o papai daqui a pouco vai ficar melhor</h1>
-      <h2>Ass: Irmão</h2>
-    </>
+    <h1>Acredite que um dia dará certo!</h1>
   );
 }
 
